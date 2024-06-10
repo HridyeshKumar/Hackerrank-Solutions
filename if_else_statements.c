40d1fa07cdf7ea27fbe18bd1a0dd09a6002283be
HRIDYESH KUMAR <153755740+HridyeshKumar@users.noreply.github.com>
2024-06-10 13:30:06 +0530

    Create if_else_statements.c

---
+++ if_else_statements.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+    int n; 
+    scanf("%d",&n);
+    if (n == 1) {
+        printf("one\n");
+    } else if (n == 2) {
+        printf("two\n");
+    } else if (n == 3) {
+        printf("three\n");
+    } else if (n == 4) {
+        printf("four\n");
+    } else if (n == 5) {
+        printf("five\n");
+    } else if (n == 6) {
+        printf("six\n");
+    } else if (n == 7) {
+        printf("seven\n");
+    } else if (n == 8) {
+        printf("eight\n");
+    } else if (n == 9) {
+        printf("nine\n");
+    } else {
+        printf("Greater than 9\n");
+    }
+    return 0;
+}

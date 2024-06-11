dcbbf1ddb34fec2a6105824f18ac4511b459db23
HRIDYESH KUMAR <153755740+HridyeshKumar@users.noreply.github.com>
2024-06-11 18:40:52 +0530

    Create basicDataTypes.cpp

---
+++ basicDataTypes.cpp
@@ -0,0 +1,15 @@
+// Basic Data Types
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    int a;
+    long b;
+    char c;
+    float d;
+    double e;
+    cin >> a >> b >> c >> d >> e;
+    cout << a << "\n" << b << "\n" << c << "\n";
+    cout << fixed << setprecision(3) << d << "\n";
+    cout << fixed << setprecision(9) << e << "\n";
+    return 0;
+}

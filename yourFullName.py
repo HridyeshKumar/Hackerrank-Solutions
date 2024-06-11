68618d85542305a1518d03ec6bb6cd9db97a3f9a
HRIDYESH KUMAR <153755740+HridyeshKumar@users.noreply.github.com>
2024-06-11 13:48:33 +0530

    Create yourFullName.py

---
+++ yourFullName.py
@@ -0,0 +1,7 @@
+#What's Your Name?
+def print_full_name(first, last):
+    print("Hello {} {}! You just delved into python.".format(first, last))
+if _name_ == '_main_':
+    first_name = input()
+    last_name = input()
+    print_full_name(first_name, last_name)

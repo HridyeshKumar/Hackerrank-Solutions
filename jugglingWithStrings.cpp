a0854467a86b72cd196306d29998adc2605bfb79
HRIDYESH KUMAR <153755740+HridyeshKumar@users.noreply.github.com>
2024-06-11 18:25:15 +0530

    Create jugglingWithStrings.cpp

---
+++ jugglingWithStrings.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() {
+    string a, b;
+    cin >> a >> b;
+    cout << a.length() << ' ' << b.length() << endl;
+    cout << a + b << endl;
+    swap(a[0], b[0]);
+    cout << a << ' ' << b << endl;
+    return 0;
+}

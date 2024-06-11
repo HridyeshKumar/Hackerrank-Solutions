2b92cfe78c90590bc6e8e7152e2710ff97d1b94c
HRIDYESH KUMAR <153755740+HridyeshKumar@users.noreply.github.com>
2024-06-11 13:31:57 +0530

    Create countingValley.py

---
+++ countingValley.py
@@ -0,0 +1,23 @@
+import math
+import os
+import random
+import re
+import sys
+def countingValleys(steps, path):
+    valleys = 0
+    cur_level = 0
+    for steps in path:
+        if(steps == 'U'):
+            cur_level += 1
+            if(cur_level == 0):
+                valleys += 1
+        elif(steps == 'D'):
+            cur_level -= 1
+    return(valleys)
+if _name_ == '_main_':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    steps = int(input().strip())
+    path = input()
+    result = countingValleys(steps, path)
+    fptr.write(str(result) + '\n')
+    fptr.close()

6b994cc60f14cf50f435ecf0810cd190e862f5d8
HRIDYESH KUMAR <153755740+HridyeshKumar@users.noreply.github.com>
2024-06-26 07:53:24 +0530

    Update alphabetRangoli.py

--- alphabetRangoli.py
+++ alphabetRangoli.py
@@ -6,7 +6,6 @@ def print_rangoli(size):
         s = "-".join(design[i:size])
         L.append((s[::-1] + s[1:]).center(4 * size - 3, "-"))
     print("\n".join(L[:0:-1] + L))
-
 if __name__ == '__main__':
     n = int(input())
     print_rangoli(n)

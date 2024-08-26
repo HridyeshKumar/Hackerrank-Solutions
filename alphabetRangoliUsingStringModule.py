62a9bf4cb98bec7871b277332327eee64f4d81d5
HRIDYESH KUMAR <153755740+HridyeshKumar@users.noreply.github.com>
2024-08-26 20:16:50 +0530

    Update alphabetRangoliUsingStringModule.py

--- alphabetRangoliUsingStringModule.py
+++ alphabetRangoliUsingStringModule.py
@@ -4,17 +4,13 @@ def print_rangoli(size):
     if size == 1:
         print(alc[0])
         return
-
     alpha_slice = [*alc[:size]]
     rangoli = []
-
     for i in range(size):
         center = alpha_slice[i]  # Isolate center letter for easy padding
         half_line = '-'.join([y for y in alpha_slice[i + 1:]])
         rangoli.append((center + half_line).center(4 * size - 3, '-'))
-
     print("\n".join(rangoli[:0:-1] + rangoli))
-
 if __name__ == '__main__':
     n = int(input())
     print_rangoli(n)

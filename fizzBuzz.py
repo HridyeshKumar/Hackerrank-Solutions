e49850e6938a9dae9aa7b5862905db6664e30ec4
HRIDYESH KUMAR <153755740+HridyeshKumar@users.noreply.github.com>
2025-03-04 01:52:48 +0530

    Add files via upload

---
+++ fizzBuzz.py
@@ -0,0 +1,17 @@
+import math
+import os
+import random
+import re
+import sys
+def fizzBuzz(n):
+    for i in range(1, n + 1):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+n = int(input().strip())
+fizzBuzz(n)
